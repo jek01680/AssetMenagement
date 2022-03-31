1a5f9483a2d8d07bf9811886ab66d546dc0c8640
jek01680 <jek01680@icloud.com>
2022-03-31 18:34:07 +0900

    Initial Commit

---
+++ MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by 정은경 on 2022/03/31.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
